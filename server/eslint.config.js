cb87cbddec494e363ff975e7497763338d51195b
edogola4 <edogola4@gmail.com>
2025-09-23 13:17:50 +0300

    feat: add ESLint flat configuration for modern code quality

---
+++ server/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettier,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'no-unused-vars': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '*.d.ts', '*.js', '!src/**/*.js'],
+  },
+];
